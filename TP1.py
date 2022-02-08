8e9cd7860d72ce4210a13217da14a17f60a1aef4
Paul NANTAS <p19nanta@pc-elec-394.priv.enst-bretagne.fr>
2022-02-08 16:33:23 +0100

    TP1 with transfer learning

--- TP1.py
+++ TP1.py
@@ -1,6 +1,6 @@
 #%%
 #Name of Model, optimizer etc.. for Graph file's name
-model_name = 'DenseNet121'
+model_name = 'DenseNet121_PT'
 optimizer_name = 'Adam'
 
 import torch
@@ -9,7 +9,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from tqdm import tqdm
 from sklearn.metrics import accuracy_score
-from models.densenet import DenseNet121
+#from models_cifar_10.densenet import DenseNet121
+from models_cifar100.densenet import DenseNet121
 
 #Model chosen from Vgg, Densenet, Resnet
 model = DenseNet121()
@@ -37,7 +38,7 @@ def train_model(model, train_loader,valid_loader,test_loader,learning_rate,  EPO
   for epoch in range(EPOCHS):
     print(f"Epoch n° : {epoch}/{EPOCHS} commencée")
     loss_train = 0
-    loss_valid = 0
+    
     #Training
     for i, data in tqdm(enumerate(train_loader, 0)):  
         inputs, labels = data
@@ -49,7 +50,9 @@ def train_model(model, train_loader,valid_loader,test_loader,learning_rate,  EPO
         #Forward + backward + optimize
         outputs = model(inputs)
         loss = criterion(outputs,labels)
+        #Calculate gradients
         loss.backward()
+        #Update weights
         optimizer.step()
         loss_train += loss.item()
         nb_batch = i     
@@ -58,6 +61,8 @@ def train_model(model, train_loader,valid_loader,test_loader,learning_rate,  EPO
     print("\n","loss par epoch train =",loss_train/(nb_batch+1))
 
     #Validation 
+    loss_valid = 0
+    model.eval()
     for i, data in tqdm(enumerate(valid_loader, 0)):  
         inputs, labels = data
         if torch.cuda.is_available():
@@ -107,9 +112,149 @@ def train_model(model, train_loader,valid_loader,test_loader,learning_rate,  EPO
     
   return model, loss_list_train,loss_list_valid, accuracy_list 
 #%%
+#Transfer Learning
 
+def pretrained_model(test_loader):
+    model =  DenseNet121()
+    dict = torch.load('models_cifar100/DenseNet121_model_cifar100_lr_0.01.pth')
+    model.load_state_dict(dict['net'])
+    model.eval()
+    model = model.to(device)
+    # Find total parameters and trainable parameters
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f'{total_params:,} total parameters.')
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    correct = 0
+    total = 0
+    with torch.no_grad():  # torch.no_grad for TESTING
+        for data in tqdm(test_loader):
+            images, labels = data
+            if torch.cuda.is_available():
+                images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            accuracy = 100 * correct / total
+    print(f'Accuracy of the network on test images: {accuracy}%')
+    return accuracy
+
+def transfer_learning(model, train_loader,valid_loader,test_loader,learning_rate,  EPOCHS,patience=30):
+    #Load of weights
+    dict = torch.load('models_cifar100/DenseNet121_model_cifar100_lr_0.01.pth')
+    model.load_state_dict(dict['net'])
+
+    n_inputs, n_classes = 1024, 4
+    #Freeze weights
+    for param in model.parameters():
+        param.requires_grad = False
+    #Replaicng the last layer with a NN
+    model.linear = nn.Sequential(
+                        nn.Linear(n_inputs, 256), 
+                        nn.ReLU(), 
+                        nn.Dropout(0.4),
+                        nn.Linear(256, n_classes),                   
+                        nn.LogSoftmax(dim=1))
+
+    # Find total parameters and trainable parameters
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f'{total_params:,} total parameters.')
+    total_trainable_params = sum(
+        p.numel() for p in model.parameters() if p.requires_grad)
+    print(f'{total_trainable_params:,} training parameters.')
+    
+    #NN parameters 
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(),lr=learning_rate)
+
+    loss_list_train = []
+    loss_list_valid = []
+    accuracy_list = []
+    early_stop = [1000,0]
+
+    for epoch in range(EPOCHS):
+        print(f"Epoch n° : {epoch}/{EPOCHS} commencée")
+        loss_train = 0
+
+        #Training
+        for i, data in tqdm(enumerate(train_loader, 0)):  
+            inputs, labels = data
+            if torch.cuda.is_available():
+                inputs, labels = inputs.to(device), labels.to(device)
+            #Clear the gradients
+            optimizer.zero_grad()
+            
+            #Forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs,labels)
+            #Calculate gradients
+            loss.backward()
+            #Update weights
+            optimizer.step()
+            loss_train += loss.item()
+            nb_batch = i     
+
+        loss_list_train.append(loss_train/i)
+        print("\n","loss par epoch train =",loss_train/(nb_batch+1))
+
+        #Validation 
+        loss_valid = 0
+        model.eval()
+        for i, data in tqdm(enumerate(valid_loader, 0)):  
+            inputs, labels = data
+            if torch.cuda.is_available():
+                inputs, labels = inputs.to(device), labels.to(device)
+            target = model(inputs)
+            # Find the Loss
+            loss = criterion(target,labels)
+            # Calculate Loss
+            loss_valid += loss.item()
+            nb_batch = i  
+            #print("\n","loss par Batch valid=",loss.item(),"\n")       
+        
+        loss_list_valid.append(loss_valid/i)
+        print("\n","loss par epoch valid =",loss_valid/(nb_batch+1))
+        #Early-Stopping
+        if loss_valid/(nb_batch+1) < early_stop[0]:
+            early_stop[0] = loss_valid/(nb_batch+1)
+            early_stop[1] = 0
+
+        else:
+            early_stop[1] += 1
+
+        print(f'Validation loss did not change for {early_stop[1]} epochs')
+
+        #Test
+        correct = 0
+        total = 0
+        with torch.no_grad():  # torch.no_grad for TESTING
+            for data in tqdm(test_loader):
+                images, labels = data
+                if torch.cuda.is_available():
+                    images, labels = images.to(device), labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                accuracy = 100 * correct / total
+        print('Accuracy of the network on test images: %d %%' % (
+            100 * correct / total))
+        accuracy_list.append(accuracy)
+
+        #End training if early stop reach the patience
+        if early_stop[1] == patience:
+            break 
+
+    
+    return model, loss_list_train,loss_list_valid, accuracy_list 
+
+#%%
 #HyperParameters
-Niter,Bsize,lr = 100 , 32, 0.00005
+Niter,Bsize,lr = 150 , 32, 0.0001
 
 #Data import
 from minicifar import minicifar_train,minicifar_test,train_sampler,valid_sampler
@@ -119,10 +264,10 @@ trainloader = DataLoader(minicifar_train,batch_size=Bsize,sampler=train_sampler)
 validloader = DataLoader(minicifar_train,batch_size=Bsize,sampler=valid_sampler)
 testloader = DataLoader(minicifar_test,batch_size=Bsize,shuffle=True) 
 
-trained_model, loss_list_train, loss_list_valid, accuracy = train_model(model,trainloader,validloader,testloader,learning_rate=lr,EPOCHS=Niter)
+train_model, loss_list_train,loss_list_valid, accuracy = transfer_learning(model, trainloader,validloader,testloader,lr,Niter)
 # %%
 
-#Register plot of Accuracy 
+#Register plot of Accuracy and loss
 import matplotlib.pyplot as plt
 #import torchvision
 import numpy as np
